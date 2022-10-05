372b279622a74cd90dc51ffdba6ae5ec0a266316
Dev-FerMorales <hufey99@hotmail>
2022-10-05 12:26:48 -0500

    Prueba guardar Github

--- 01.js
+++ 01.js
@@ -1,5 +1,5 @@
 /* 
-  Importante: 
+  Importante:  :)
   No modificar ni el nombre ni los argumetos que reciben las funciones, sólo deben escribir
   código dentro de las funciones ya definidas. 
   No comentar la funcion 
